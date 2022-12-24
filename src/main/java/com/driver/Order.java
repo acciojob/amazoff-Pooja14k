21d48f73cc7d1a668ade876b19013ab431d80799
Pooja Kale <99339372+Pooja14k@users.noreply.github.com>
2022-12-25 00:01:56 +0530

    Update Order.java

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -9,6 +9,9 @@ public class Order {
 
         // The deliveryTime has to converted from string to int and then stored in the attribute
         //deliveryTime  = HH*60 + MM
+        int totalTime= Integer.valueOf(deliveryTime.substring(0,2))*60+Integer.valueOf(deliveryTime.substring(3,5));
+        this.id=id;
+        this.deliveryTime=totalTime;
     }
 
     public String getId() {
